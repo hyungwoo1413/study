06c0db749ac7ed712a2c2a79ea98fc61a49729ef
hyungwoo1413 <glhwguddn@gmail.com>
2025-02-25 17:14:40 +0900

    2025-02-25

--- 5_project/mini_game_heaven.py
+++ 5_project/mini_game_heaven.py
@@ -1,7 +1,4 @@
-import os
-import keyboard
 import bingo
-import time
 ### CLI 
 
 """
@@ -10,47 +7,22 @@ UI부터 만들어도 됩니다.
 
 CLI UI가 별거없음.
 """
-def clearScreen(): # os에 특화된 팁
-    command = 'clear'
-    if os.name in ('nt', 'dos'):
-        command = 'cls'
-    os.system(command)
 
-def check(num, select):
-    point = '<--'
-    if num == select:
-        return point
-    return ''
 
-def main():
-    num = 1
-    while True:
-        clearScreen()
-        print('강황석준 조의 미니게임 천국에 오신걸 환영합니다!')
-        print()
-        print('메뉴선택')
-        print(f'1: 가위바위보게임 {check(1, num)}')
-        print(f'2: 빙고게임 {check(2, num)}')
-        print(f'3: 프로그램 종료 {check(3, num)}')
-        time.sleep(0.1)
-        
-        if keyboard.is_pressed('down'): 
-            if num < 3:
-                num += 1
-            time.sleep(0.1)
-        elif keyboard.is_pressed('up'):
-            if num > 1:
-                num -= 1
-            time.sleep(0.1)
-        if keyboard.is_pressed('enter'):
-            if num == 1 : 
-                print('가위바위보')
-                time.sleep(1)
-            elif num == 2 :
-                bingo.bingo()
-            elif num == 3 :
-                print('프로그램을 종료합니다.')
-                break
+while True:
+    print('강황석준 조의 미니게임 천국에 오신걸 환영합니다!')
+    print('메뉴선택')
+    print('1: 가위바위보게임 | 2: 빙고게임 | 3: 프로그램 종료')
 
-if __name__ == "__main__":
-    main()
+    command = int(input())
+
+    if command == 1 : 
+        # 가위바위보 게임 시작
+        print('가위바위보')
+        pass
+    elif command == 2 :
+        bingo.bingo()       # import 된 bingo.py 안의 bingo 함수를 실행한다.
+        pass
+    elif command == 3 : # 종료
+        print('프로그램을 종료합니다.')
+        break
